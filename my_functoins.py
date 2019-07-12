2551011dc9c26533888b1634d8fb620f40d81f14
java2019 <java_2019@mail.ru>
2019-07-12 13:51:15 +0300

    Initial commit

---
+++ my_functoins.py
@@ -0,0 +1,10 @@
+def print_name(my_name):
+    print(my_name)
+
+
+def print_name_plus_num(my_name):
+    print(my_name, 123)
+
+
+def my_sum(num1, num2):
+    print(num1 + num2)
---
+++ welcome.py
@@ -0,0 +1,20 @@
+import sys
+import random
+import my_functoins
+
+print("Hello world")
+print(1+1)
+str = "Any string"
+print(str[2:8])
+print(sys.version)
+print(random.randint(1,5))
+print(str.islower())
+nums = [2]
+nums.extend([3,4])
+print(nums)
+nums.remove(3)
+print(nums)
+
+
+my_functoins.my_sum(2,3)
+my_functoins.print_name_plus_num("Anna")
